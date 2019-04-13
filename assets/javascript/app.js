0c2bb3d254797c55693ebcb9b4bd7fe5f6450591
Robert Ross <rob@campus-076-236.ucdavis.edu>
2019-04-13 13:45:04 -0700

    working ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
 
 // var doSearch = $("#search").text();
-    dosearch = "antique cars"
-
+    var doSearch = "antique cars"
+console.log(doSearch);
     $("#buttonSearch").on("click", function() {
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-          doSearch + "&api-key=e959f026-d4a2-411a-a3e8-070ba9b7591a";  
+          doSearch + "&api-key=xR4oAoZ4aTxWA1npAMBXyfGRnQ4sgoeB";  
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       console.log(response);
 
+      $("#web_url").text("Article Link: " + response.web_url);
+      $("#headline").text("Headline: " + response.headline);
+      $("#pub_date").text("Publish Date: " + response.pub_date);
+      $("#lead_paragraph").text("Introduction: " + response.lead_pragraph);
     });
 });
 });
